f7d1b4c60f89253c0938ca0cc37ab42b66ecf9c0
dinarka <dinarka.d@gmail.com>
2018-11-30 15:05:15 +0600

    Update task_4_4.rb

--- task_4_4.rb
+++ task_4_4.rb
@@ -1,15 +1,15 @@
 #Заполнить хеш гласными буквами, где значением будет являться порядковый номер буквы в алфавите (например: А - 1).
-arr = ('a'..'z').to_a
+alphabet = ('a'..'z').to_a
 vowels = ['a','e','i','o','u']
-vow_hash = {}
+vowel_hash = {}
 
-arr.each_with_index do |val, i|
+alphabet.each_with_index do |val, i|
   vowels.each do |vowel|
     if val == vowel
-      vow_hash[i + 1] = val
+      vowel_hash[i + 1] = val
     end
   end
 end
 
 
-p vow_hash
+p vowel_hash
